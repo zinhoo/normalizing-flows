3f4b4224a3952b548c8b0ec2649df417d1fcd108
Brian Groenke <brian.groenke@colorado.edu>
2019-12-09 06:12:05 +0000

    Add lazy per-step flow forward impl

--- flows/flow.py
+++ flows/flow.py
@@ -27,6 +27,22 @@ class Flow():
             z_k, ldj_k = transform.forward(z_k, args[:,i])
             ldj += ldj_k
         return z_k, ldj
+    
+    def lazy_forward(self, z, args: tf.Tensor):
+        """
+        Lazily computes the forward pass of the flow: z_k = f_k . f_k-1 ... f_1(z) step by step.
+
+        Tensor shapes:
+        z    : (batch_size, d)
+        args : (batch_size, m) where m is equal to the total number of parameters for all flows
+        """
+        n_flows, n_params = self.num_flows, self.param_count(tf.shape(z)[1])
+        args = tf.reshape(args, (-1, n_flows, n_params // n_flows))
+        z_k = z
+        yield z_k, 0.0
+        for i, transform in enumerate(self.transforms):
+            z_k, ldj_k = transform.forward(z_k, args[:,i])
+            yield z_k, ldj_k
 
     def param_count(self, d):
         return sum([t.param_count(d) for t in self.transforms])
